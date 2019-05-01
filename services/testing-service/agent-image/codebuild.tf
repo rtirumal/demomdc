2414b282c8d3cbaaf78e195064f8edc7086ec5b2
rtirumal <41272405+rtirumal@users.noreply.github.com>
2019-05-01 02:44:59 -0500

    Update codebuild.tf

--- services/testing-service/agent-image/codebuild.tf
+++ services/testing-service/agent-image/codebuild.tf
@@ -22,6 +22,7 @@ resource "aws_codebuild_project" "agent_build" {
   name          = "${var.service_name}-${var.service_role}-build-image-p${var.php_version_short}"
   build_timeout = "120"
   description = "Build Jenkins agent docker image for p${var.php_version_short}"
+  service_role  = "${var.service_role}"
   service_role  = "${data.terraform_remote_state.acct_level_resources.codebuild_role_agent_build_arn}"
 
   artifacts {
@@ -38,6 +39,7 @@ resource "aws_codebuild_project" "agent_build" {
   source {
     type      = "S3"
     buildspec = "${data.template_file.buildspec.rendered}"
-    location = "${data.terraform_remote_state.acct_level_resources.placeholder_bucket_id}/placeholder.zip"
+    #location = "${data.terraform_remote_state.acct_level_resources.placeholder_bucket_id}/placeholder.zip"
+    location = "${var.acct_level_resources_s3_bucket}/placeholder.zip"
   }
 }
