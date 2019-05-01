71b55d7cee52e2acb0f262ecffae65f49644962d
Tirumal Tunga <ttunga@ip-192-168-1-16.ec2.internal>
2019-05-01 00:27:52 -0500

    code1

--- services/testing-service/agent-image/main.tf
+++ services/testing-service/agent-image/main.tf
@@ -13,21 +13,21 @@ provider "aws" {
 # CONFIGURE REMOTE STATE STORAGE
 # ---------------------------------------------------------------------------------------------------------------------
 
-#terraform {
+terraform {
   # The configuration for this backend will be filled in by Terragrunt
- # backend "s3" {}
+  backend "s3" {}
 
   # Only allow this Terraform version. Note that if you upgrade to a newer version, Terraform won't allow you to use an
   # older version, so when you upgrade, you should upgrade everyone on your team and your CI servers all at once.
-#  required_version = "~> 0.11.10"
-#}
+  required_version = "~> 0.11.10"
+}
 
-#data "terraform_remote_state" "acct_level_resources" {
-#backend = "s3"
-#
-#config {
-#  region = "${var.acct_level_resources_region}"
-#  bucket = "${var.acct_level_resources_s3_bucket}"
-#  key    = "${var.acct_level_resources_s3_key}/terraform.tfstate"
-#  }
-#}
+data "terraform_remote_state" "acct_level_resources" {
+backend = "s3"
+
+config {
+  region = "${var.acct_level_resources_region}"
+  bucket = "${var.acct_level_resources_s3_bucket}"
+  key    = "${var.acct_level_resources_s3_key}/terraform.tfstate"
+  }
+}
--- services/testing-service/agent-image/variables.tf
+++ services/testing-service/agent-image/variables.tf
@@ -27,20 +27,20 @@ variable "service_role" {
   default     = "agent"
 }
 
-#variable "acct_level_resources_s3_bucket" {
- # description = "Account level S3 remote state bucket name"
- # default     = "demos3bucket"
-#}
+variable "acct_level_resources_s3_bucket" {
+  description = "Account level S3 remote state bucket name"
+  default     = "demos3bucket"
+}
 
-#variable "acct_level_resources_s3_key" {
- # description = "Account level S3 remote state S3 key path"
-  #default     = "/usr/bin/s3"
-#}
+variable "acct_level_resources_s3_key" {
+  description = "Account level S3 remote state S3 key path"
+  default     = "/usr/bin/s3"
+}
 
-#variable "acct_level_resources_region" {
-#  description = "Account level remote state region"
-#  default     = "us-east-1"
-#}
+variable "acct_level_resources_region" {
+  description = "Account level remote state region"
+  default     = "us-east-1"
+}
 
 ######################################
 # Codebuild
